341bff13fba9c642dd002f8eef024c2f4fb6780c
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-05-01 19:14:38 +0200

    wip seq

---
+++ zic/zic_seq_loop.h
@@ -0,0 +1,112 @@
+#ifndef ZIC_LOOP_H_
+#define ZIC_LOOP_H_
+
+#include "zic/zic_note.h"
+#include "zic/zic_seq_pattern.h"
+
+#define REF_NOTE _C4
+
+class Zic_Seq_Loop
+{
+protected:
+    uint8_t currentStep = 0;
+    Zic_Seq_Step stepOn;
+    Zic_Seq_Step stepOff;
+    uint8_t velocity = 100;
+    uint8_t nextVelocity = 100;
+
+    uint8_t nextToPlay = 0;
+
+    void activateSingleLoopMode(bool value = true)
+    {
+        loopOn = value;
+        if (loopOn)
+        {
+            nextToPlay = previousLoopNote;
+        }
+        else
+        {
+            previousLoopNote = nextToPlay;
+            nextToPlay = 0;
+        }
+    }
+
+    virtual void setNextToPlay()
+    {
+        play = nextToPlay ? nextToPlay : 0;
+    }
+
+public:
+    bool loopOn = false;
+    uint8_t play = 0;
+    uint8_t previousLoopNote = 0;
+
+    Zic_Seq_Pattern *nextPattern = NULL;
+    Zic_Seq_Pattern *pattern = NULL;
+
+    Zic_Seq_Loop() {}
+    Zic_Seq_Loop(Zic_Seq_Pattern *_pattern)
+    {
+        pattern = _pattern;
+        nextPattern = _pattern;
+    }
+
+    void setNextPattern(Zic_Seq_Pattern *_nextPattern)
+    {
+        nextPattern = _nextPattern;
+    }
+
+    Zic_Seq_Step *getNoteOn()
+    {
+        if (play && stepOn.note > 0)
+        {
+            return &stepOn;
+        }
+        return NULL;
+    }
+
+    Zic_Seq_Step *getNoteOff()
+    {
+        if (!stepOff.slide)
+        {
+            // To avoid repeating this again, let set slide to true
+            stepOff.slide = true;
+        }
+        return NULL;
+    }
+
+    void next()
+    {
+        if (pattern)
+        {
+            stepOff.set(&stepOn);
+            stepOn.set(&pattern->steps[currentStep]);
+            currentStep = (currentStep + 1) % pattern->stepCount;
+        }
+
+        if (currentStep == 0)
+        {
+            pattern = nextPattern;
+            setNextToPlay();
+            velocity = nextVelocity;
+        }
+    }
+
+    void noteOn(uint8_t note, uint8_t _velocity)
+    {
+        nextVelocity = _velocity;
+        nextToPlay = note;
+    }
+
+    void noteOff(uint8_t note)
+    {
+        if (!loopOn && note == nextToPlay)
+        {
+            nextToPlay = 0;
+        }
+    }
+
+    void toggleMode() { activateSingleLoopMode(!loopOn); }
+};
+
+#endif
---
+++ zic/zic_seq_pattern.h
@@ -0,0 +1,102 @@
+#ifndef ZIC_SEQ_PATTERN_H_
+#define ZIC_SEQ_PATTERN_H_
+
+#define MAX_STEPS_IN_PATTERN 64
+#define PATTERN_NAME_SIZE 15
+#define SLIDE 1
+#define END 1
+
+typedef uint8_t Pat[MAX_STEPS_IN_PATTERN][2];
+
+class Zic_Seq_Step
+{
+public:
+    uint8_t note = 0;
+    uint8_t velocity = 127;
+    bool slide = false;
+
+    Step() {}
+
+    Zic_Seq_Step(uint8_t _note, bool _slide)
+    {
+        note = _note;
+        slide = _slide;
+    }
+
+    void reset()
+    {
+        note = 0;
+        velocity = 127;
+        slide = false;
+    }
+
+    void set(Zic_Seq_Step *step)
+    {
+        set(step->note, step->velocity, step->slide);
+    }
+
+    void set(uint8_t _note)
+    {
+        note = _note;
+    }
+
+    void set(uint8_t _note, uint8_t _velocity)
+    {
+        velocity = _velocity;
+        set(_note);
+    }
+
+    void set(uint8_t _note, bool _slide)
+    {
+        slide = _slide;
+        set(_note);
+    }
+
+    void set(uint8_t _note, uint8_t _velocity, bool _slide)
+    {
+        slide = _slide;
+        set(_note, _velocity);
+    }
+};
+
+class Zic_Seq_Pattern
+{
+public:
+    uint8_t stepCount = MAX_STEPS_IN_PATTERN;
+    Zic_Seq_Step steps[MAX_STEPS_IN_PATTERN];
+
+    Zic_Seq_Pattern(Pat _steps)
+    {
+        for (uint8_t pos = 0; pos < MAX_STEPS_IN_PATTERN; pos++)
+        {
+            if (_steps[pos][0] == END)
+            {
+                stepCount = pos;
+                break;
+            }
+            steps[pos].set(_steps[pos][0], _steps[pos][1] == 1);
+        }
+    }
+
+    // for testing
+    void print()
+    {
+        for (uint8_t x = 0; x < stepCount; x++)
+        {
+            Serial.print("[");
+            Serial.print(x);
+            Serial.print(", ");
+            Serial.print(steps[x].note);
+            Serial.print(", ");
+            Serial.print(steps[x].slide);
+            Serial.print("],");
+            if (x % 8 == 7)
+            {
+                Serial.println("");
+            }
+        }
+        Serial.println("\n");
+    }
+};
+
+#endif
